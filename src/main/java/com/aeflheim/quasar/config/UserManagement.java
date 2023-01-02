b60f3eb40dbd79c30fd13e22484ba6e0c9794835
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-02 05:38:26 +0100

    add: userdetailsservice implementation

--- src/main/java/com/aeflheim/quasar/config/UserManagement.java
+++ src/main/java/com/aeflheim/quasar/config/UserManagement.java
@@ -1,33 +1,32 @@
 package com.aeflheim.quasar.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import com.aeflheim.quasar.model.User;
+import com.aeflheim.quasar.service.InMemoryUserDetailsService;
 
 @Configuration
 public class UserManagement {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails userDetails = new User("king", "infinity", "READ");
+        UserDetails userDetails1 = new User("anna", "sandstorm", "READ");
+        List<UserDetails> users = List.of(userDetails, userDetails1);
 
-        var user = User.withUsername("aeon")
-                .password(passwordEncoder().encode("regulus"))
-                .roles("read")
-                .build();
-
-        userDetailsService.createUser(user);
-
-        return userDetailsService;
+        return new InMemoryUserDetailsService(users);
 
     }
 }
--- src/main/java/com/aeflheim/quasar/model/User.java
+++ src/main/java/com/aeflheim/quasar/model/User.java
@@ -1,52 +1,58 @@
 package com.aeflheim.quasar.model;
 
-public class User {
+import java.util.Collection;
+import java.util.List;
 
-    private Long id;
-    private String username;
-    private String password;
-    private String authority;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-    public User(Long id, String username, String password, String authority) {
-        this.id = id;
+public class User implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final String authority;
+
+    public User(String username, String password, String authority) {
         this.username = username;
         this.password = password;
         this.authority = authority;
-    }
-
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getAuthority() {
         return authority;
     }
 
-    public void setAuthority(String authority) {
-        this.authority = authority;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> authority);
+    }
+
+    @Override   
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
