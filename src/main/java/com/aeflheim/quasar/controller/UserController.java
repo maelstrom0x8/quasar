d15b449deb015da30ae0a8879b8f3643c3c6db87
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-02 20:04:01 +0100

    ssia-c3-final

--- src/main/java/com/aeflheim/quasar/controller/UserController.java
+++ src/main/java/com/aeflheim/quasar/controller/UserController.java
@@ -1,24 +1,25 @@
 package com.aeflheim.quasar.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aeflheim.quasar.dto.UserDto;
-import com.aeflheim.quasar.service.QsUserService;
+import com.aeflheim.quasar.service.QsUserDetailsService;
 
 @RestController
 @RequestMapping("/auth")
 public class UserController {
 
-    @Autowired
-    private QsUserService qsUserService;
+    private QsUserDetailsService userDetailsService;
 
-    
-    public String createUser(UserDto userDto) {
-        return qsUserService.createUser(userDto);
+    public UserController(QsUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
-
+    @PostMapping("/register")
+    public String createUser(UserDto userDto) {
+        return userDetailsService.createUser(userDto);
+    }
 
 }
--- src/main/java/com/aeflheim/quasar/service/QsUserService.java
+++ src/main/java/com/aeflheim/quasar/service/QsUserService.java
@@ -2,14 +2,12 @@ package com.aeflheim.quasar.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.aeflheim.quasar.dto.UserDto;
 import com.aeflheim.quasar.model.User;
 import com.aeflheim.quasar.repository.UserRepository;
 
-@Service
+
 public class QsUserService {
 
     @Autowired
@@ -18,7 +16,6 @@ public class QsUserService {
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping("/register")
     public String createUser(UserDto userDto) {
         User user = new User(
                 userDto.getUsername(),
