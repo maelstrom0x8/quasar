f4a47bc2a0c20ea65039f75eca90c6c0595217be
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-05 04:58:48 +0100

    rename file

--- src/main/java/com/aeflheim/quasar/config/WebSecurityConfig.java
+++ src/main/java/com/aeflheim/quasar/config/WebSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
