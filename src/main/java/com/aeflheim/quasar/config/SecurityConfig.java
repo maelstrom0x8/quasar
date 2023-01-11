f0e26e6f5a62d077e14200dcd719e041349ecfc7
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-11 02:55:21 +0100

    oauth-e2

--- src/main/java/com/aeflheim/quasar/config/SecurityConfig.java
+++ src/main/java/com/aeflheim/quasar/config/SecurityConfig.java
@@ -3,34 +3,14 @@ package com.aeflheim.quasar.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        var c = clientRegistration();
-        return new InMemoryClientRegistrationRepository(c);
-    }
-
-    private ClientRegistration clientRegistration() {
-        return CommonOAuth2Provider.GITHUB
-                .getBuilder("github")
-                .clientId("7eae9a615eeef3e97ef4")
-                .clientSecret("cd4a14dacaa96895966b03a6992e4fbd748b519c")
-                .build();
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.oauth2Login(c -> {
-            c.clientRegistrationRepository(clientRegistrationRepository());
-        });
+        http.oauth2Login();
 
         http.authorizeHttpRequests(auth -> {
             auth.anyRequest().authenticated();
