9b0d011590ac6cdcdc22992baabdd7a98698c1a7
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-11 02:40:39 +0100

    oauth-e1

--- src/main/java/com/aeflheim/quasar/config/SecurityConfig.java
+++ src/main/java/com/aeflheim/quasar/config/SecurityConfig.java
@@ -2,29 +2,41 @@ package com.aeflheim.quasar.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        var c = clientRegistration();
+        return new InMemoryClientRegistrationRepository(c);
+    }
+
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider.GITHUB
+                .getBuilder("github")
+                .clientId("7eae9a615eeef3e97ef4")
+                .clientSecret("cd4a14dacaa96895966b03a6992e4fbd748b519c")
+                .build();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> {
+        http.oauth2Login(c -> {
+            c.clientRegistrationRepository(clientRegistrationRepository());
+        });
+
+        http.authorizeHttpRequests(auth -> {
             auth.anyRequest().authenticated();
-        })
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(Customizer.withDefaults())
-                .build();
+        });
+
+        return http.build();
     }
 
 }
---
+++ src/main/java/com/aeflheim/quasar/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.aeflheim.quasar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main.html";
+    }
+}
