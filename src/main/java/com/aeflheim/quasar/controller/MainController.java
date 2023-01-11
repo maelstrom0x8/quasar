1c6743058157ba10174bd4600ab8cdbf03ef57b4
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-11 03:16:58 +0100

    ssia-ch12-final

--- src/main/java/com/aeflheim/quasar/controller/MainController.java
+++ src/main/java/com/aeflheim/quasar/controller/MainController.java
@@ -1,13 +1,20 @@
 package com.aeflheim.quasar.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    private Logger logger = LoggerFactory.getLogger(MainController.class.getName());
+
     @GetMapping("/")
-    public String main() {
+    public String main(OAuth2AuthenticationToken token) {
+        logger.info(String.valueOf(token.getPrincipal()));
+        
         return "main.html";
     }
 }
